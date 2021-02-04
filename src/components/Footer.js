a33238e82268757654525e11a42c6bff1f6f6baa
Alex <alex_15_zhen@hotmail.com>
2021-02-03 22:11:57 -0500

    Footer props

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,63 +1,45 @@
 import React from "react";
 import "./Footer.css";
+import FooterIcon from "./FooterIcon";
 
 function Footer() {
   return (
-    <div className='footer-container' id="contact">
-      <section className='footer-sub'>
-        <p className='footer-heading'>
-          Feel free to contact me for anything.
-        </p>
-        <p className='footer-text'>
-          Or just simply say "Hi!"
-        </p>
+    <div className="footer-container" id="contact">
+      <section className="footer-sub">
+        <p className="footer-heading">Feel free to contact me for anything.</p>
+        <p className="footer-text">Or just simply say "Hi!"</p>
       </section>
-      <section class='social-media'>
-        <div class='social-media-wrap'>
-          <div class='social-icons'>
-            <a
-              class='social-icon-link Facebook'
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="social-icons">
+            <FooterIcon
               href="https://www.facebook.com/alexander.zhen/"
-              target='_blank'
-              aria-label='Facebook'
-            >
-              <i class='fab fa-facebook-f' />
-            </a>
-            <a
-              class='social-icon-link Instagram'
+              label="Facebook"
+              icon="fab fa-facebook-square"
+            />
+            <FooterIcon
               href="https://www.instagram.com/just.az_/"
-              target='_blank'
-              aria-label='Instagram'
-            >
-              <i class='fab fa-instagram' />
-            </a>
-            <a
-              class='social-icon-link GitHub'
+              label="Instagram"
+              icon="fab fa-instagram"
+            />
+            <FooterIcon
               href="https://github.com/alexzhen93"
-              target='_blank'
-              aria-label='GitHub'
-            >
-              <i class='fab fa-github' />
-            </a>
-            <a
-              class='social-icon-link LinkedIn'
+              label="GitHub"
+              icon="fab fa-github-square"
+            />
+            <FooterIcon
               href="https://www.linkedin.com/in/jia-peng-zhen-6455a1170/"
-              target='_blank'
-              aria-label='LinkedIn'
-            >
-              <i class='fab fa-linkedin' />
-            </a>
-            <a
-              class='social-icon-link E-Mail'
+              label="LinkedIn"
+              icon="fab fa-linkedin"
+            />
+            <FooterIcon
               href="mailto:Jiapengzhen@outlook.com"
-              target='_blank'
-              aria-label='E-Mail'
-            >
-              <i class='far fa-envelope' />
-            </a>
+              label="E-Mail"
+              icon="far fa-envelope"
+            />
           </div>
         </div>
-        <small class='website-rights'>Alex Zhen © 2021</small>
+        <small className="website-rights">Alex Zhen © 2021</small>
       </section>
     </div>
   );
